83d6c554075a0dc64ac140369fe3dfde1551c30e
JasonSignatures <tunenjack2003@gmail.com>
2022-06-30 15:37:42 -0700

    print using prinf, return o.compile without warning

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ *
+ * Return: Always 0 (Succeess)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a pieceof art,\n");
+	return (0);
+}
