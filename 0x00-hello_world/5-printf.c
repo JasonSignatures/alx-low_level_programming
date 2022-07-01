c4b33c48f57be406fc15d37fe7d9aafd898139ee
JasonSignatures <tunenjack2003@gmail.com>
2022-06-30 19:08:07 -0700

    print using printf, return 0. compile without warning

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-gcc "-Wall -Werror -Wextra -pedantic -std=gnu89 ./a.out"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
